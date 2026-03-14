b79a14cabd3555cf97fa562a5de40a079e7ca742
Killian <killianpurcell2010@gmail.com>
2026-03-14 21:17:15 +0000

    Fix: Update Groq vision model to Llama 4 Scout (vision models deprecated)

--- api/identify-fish.js
+++ api/identify-fish.js
@@ -35,7 +35,7 @@ module.exports = async function handler(req, res) {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                model: 'llama-3.2-90b-vision-preview',
+                model: 'meta-llama/llama-4-scout-17b-16e-instruct',
                 messages: [
                     {
                         role: 'user',
